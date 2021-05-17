ab83ea4a607780e6dbb4d17d1bbf66b13d9b7403
mwashief <muwashief@gmail.com>
2021-05-17 23:31:17 +0600

    navbar set up

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,77 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <b-navbar toggleable="lg" class="shadow-sm">
+      <b-navbar-brand to="/">
+        <img
+          src="./assets/iclp_logo.png"
+          class="d-inline-block align-top navbar-logo"
+          alt="ICLP"
+        />
+      </b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="/about">Courses</b-nav-item>
+          <b-nav-item to="/">Blog</b-nav-item>
+          <b-nav-item to="/">Chellenges</b-nav-item>
+          <b-nav-item to="/">Profile</b-nav-item>
+          <b-nav-item to="/">Instructors</b-nav-item>
+        </b-navbar-nav>
+
+        <b-navbar-nav class="ml-auto" v-if="profile">
+          <b-nav-item to="/">Messages</b-nav-item>
+        </b-navbar-nav>
+        <b-navbar-nav v-if="profile">
+          <b-nav-item-dropdown right>
+            <template #button-content>
+              <b-avatar variant="info" :src="profile"> </b-avatar>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item @click="signOut">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+        <b-navbar-nav class="ml-auto" v-else>
+          <b-nav-item>Log In</b-nav-item>
+          <b-nav-item>Sign Up</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+
+    <b-container>
+      <router-view />
+    </b-container>
   </div>
 </template>
 
+
+<script>
+export default {
+  data() {
+    return {
+      profile:"https://placekitten.com/300/300",
+    };
+  },
+  methods: {
+    signOut() {
+      console.log("Signing out....");
+      this.profile = ""
+    },
+  },
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+$navbar-light-color: rgba(0, 0, 0, 1);
+$navbar-light-hover-color: rgba(200, 69, 0, 1);
+$navbar-light-active-color: rgba(200, 69, 0, 1);
+//$nav-link-padding-y: 0.3rem;
+// Bootstrap and its default variables
+@import "../node_modules/bootstrap/scss/bootstrap";
+// BootstrapVue and its default variables
+@import "../node_modules/bootstrap-vue/src/index.scss";
+.navbar-logo {
+  width: 150px;
 }
 </style>
