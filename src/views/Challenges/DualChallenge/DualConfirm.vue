8ce018360f5b2e311ab0709a841ee1ac361ba087
Ahsanul-Ameen <ahsanulsabit7@gmail.com>
2021-05-20 02:37:35 +0600

    Adjust Browse Topics from Dual Challenge

---
+++ src/views/Challenges/DualChallenge/DualConfirm.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+	<b-container>  
+    <h1>This Page is eaten by me...</h1> 
+    <b-img src="https://rainydaymum.co.uk/wp-content/uploads/2015/09/floating-ghosts-6.jpg" fluid alt="Responsive image"></b-img>
+  </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { 
+      topicId: ""
+    };
+  },
+  methods: { },
+  created() {
+    this.topicId = this.$route.params.topicId;
+  },
+
+};
+</script>
+
+<style lang="scss" scoped></style>
