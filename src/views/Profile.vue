72c2d5a62e3fe76f924e7d86d0d85cd7947ca19c
Nafiur Rahman Khadem <shafinkhadem@gmail.com>
2021-06-16 22:05:58 +0600

    add bar plot of daily best attempts

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -8,15 +8,15 @@
       </b-form-group>
       Rank: {{rank}}
       Total score: {{total_score}}
-      <div class="small">
-        <line-chart :chart-data="datacollection" :options="chartoptions"></line-chart>
-      </div>
+      <line-chart :chart-data="linedata" :options="chartoptions" />
+      <bar-chart :chart-data="bardata" :options="chartoptions" />
     </div>
   </div>
 </template>
 
 <script>
 import LineChart from "@/components/LineChart.js";
+import BarChart from "@/components/BarChart.js";
 import apiUtil from "@/mixins/apiUtil";
 import moment from "moment";
 
@@ -26,13 +26,15 @@ export default {
   mixins: [apiUtil],
   components: {
     LineChart,
+    BarChart,
   },
   data() {
     return {
       user: null,
       topics: [],
       topicid: 0,
-      datacollection: {},
+      linedata: {},
+      bardata: {},
       chartoptions: {
         maintainAspectRatio: false,
         scales: {
@@ -41,6 +43,18 @@ export default {
               type: "time",
               time: {
                 unit: "day",
+              },
+
+              // unique defaults for bar chart which doesn't get merged once you specify xAxes :)
+              offset: true,
+              gridLines: { offsetGridLines: true },
+            },
+          ],
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+                precision: 0,
               },
             },
           ],
@@ -64,17 +78,34 @@ export default {
     fillData() {
       this.apiGet(`/public/best/${this.id}?topicid=${this.topicid}`).then(
         (data) => {
-          const bgn = { ...data[0] };
-          bgn.score = 0;
-          data.unshift(bgn);
-          const plotdata = data.map(
+          // const bgn = { ...data[0] };
+          // bgn.score = 0;
+          // data.unshift(bgn);
+          const lineplotdata = data.map(
             ((s) => (v) => ({ x: moment(v.time), y: (s += v.score) }))(0)
           );
-          this.datacollection = {
+          const barplotdata = data.map((v) =>
+            moment(v.time).startOf("day").format("YYYY-MM-DD")
+          );
+          const counts = {};
+          for (var i = 0; i < barplotdata.length; i++) {
+            var num = barplotdata[i];
+            counts[num] = (counts[num] || 0) + 1;
+          }
+          this.linedata = {
             datasets: [
               {
                 label: "Cumulative score",
-                data: plotdata,
+                data: lineplotdata,
+              },
+            ],
+          };
+          this.bardata = {
+            labels: Object.keys(counts),
+            datasets: [
+              {
+                label: "Best attempts",
+                data: Object.values(counts),
               },
             ],
           };
@@ -99,8 +130,4 @@ export default {
 </script>
 
 <style>
-.small {
-  max-width: 600px;
-  margin: 150px auto;
-}
 </style>
