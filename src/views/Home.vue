1a3d2f78e63af793189a5e2243bc8577c7441102
Nafiur Rahman Khadem <shafinkhadem@gmail.com>
2022-01-08 00:33:58 +0600

    Update homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,19 @@
 <template>
-  <b-jumbotron
-    header="Interactive Code Learning Platform"
-    lead="Software Dev Project"
-    ><b-button-group class="mt-5" v-if="!this.$store.getters.isLoggedIn">
-      <b-button variant="primary" :to="{ name: 'Signup' }">Join Us</b-button>
-      <b-button variant="dark" :to="{ name: 'Login' }">Login</b-button>
-    </b-button-group>
-  </b-jumbotron>
+  <div>
+    <b-jumbotron
+      header="Programming Knowledge Tester"
+      lead="A website to test programming knowledge with MCQs and programming problems with an automatic judging system."
+    >
+      <b-button-group class="mt-5" v-if="!this.$store.getters.isLoggedIn">
+        <b-button variant="primary" :to="{ name: 'Signup' }">Join Us</b-button>
+        <b-button variant="dark" :to="{ name: 'Login' }">Login</b-button>
+      </b-button-group>
+    </b-jumbotron>
+    Frontend source code is available
+    <a href="https://github.com/mwashief/ICLP"> at github</a>. We created a REST
+    API using Node.js & PostgreSQL database in the backend; used Vue.js, Vuex &
+    bootstrap-vue in the frontend. Backend is deployed using docker.
+  </div>
 </template>
 
 <script>
