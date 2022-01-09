4f03034ae01a97d1dbbe0a123b0b0d9483d5f56a
Nafiur Rahman Khadem <shafinkhadem@gmail.com>
2022-01-10 01:18:58 +0600

    add repository links in homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,10 +10,29 @@
         <b-button variant="dark" :to="{ name: 'Login' }">Login</b-button>
       </b-button-group>
     </b-jumbotron>
-    Frontend source code is available
-    <a href="https://github.com/mwashief/ICLP"> at github</a>. We created a REST
-    API using Node.js & PostgreSQL database in the backend; used Vue.js, Vuex &
-    bootstrap-vue in the frontend. Backend is deployed using docker.
+    <b-row class="my-5">
+      <b-col cols="12" md="6" class="text-center">
+        <a href="https://github.com/mwashief/ICLP" target="blank">
+          <img
+            src="https://github-readme-stats.vercel.app/api/pin/?username=mwashief&repo=ICLP"
+            class="mw-100"
+          />
+        </a>
+      </b-col>
+      <b-col class="text-center">
+        <a href="https://github.com/mwashief/iclp-api" target="blank">
+          <img
+            src="https://github-readme-stats.vercel.app/api/pin/?username=mwashief&repo=iclp-api"
+            class="mw-100"
+          />
+        </a>
+      </b-col>
+    </b-row>
+    <p>
+      We created a REST API using Node.js & PostgreSQL database in the backend;
+      used Vue.js, Vuex & bootstrap-vue in the frontend. Backend is deployed
+      using docker.
+    </p>
   </div>
 </template>
 
